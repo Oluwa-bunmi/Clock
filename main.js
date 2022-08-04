bd0d9636c08294aebc730c50c4c69ed33bf11ae7
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-08-04 14:17:32 +0100

    declared variables

---
+++ main.js
@@ -0,0 +1,7 @@
+let hour = document.getElementById("hrs");
+let minute = document.getElementById("min");
+let second = document.getElementById("sec");
+let day = new Date();
+let hr = day.getHours() * 30;
+let mm = day.getMinutes() * 6;
+let ss = day.getSeconds() * 6;
