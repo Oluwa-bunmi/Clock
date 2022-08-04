ac330af3efb512566e58af05f114f6b6992ac9f6
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-08-04 14:47:01 +0100

    done with analog clock

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let hour = document.getElementById("hrs");
+let hour = document.getElementById("hr");
 let minute = document.getElementById("min");
 let second = document.getElementById("sec");
+
+
+setInterval(() =>{
 let day = new Date();
 let hr = day.getHours() * 30;
 let mm = day.getMinutes() * 6;
 let ss = day.getSeconds() * 6;
+
+hour.style.transform = `rotateZ(${hr + mm / 12}deg)`;
+minute.style.transform = `rotateZ(${mm}deg)`;
+second.style.transform = `rotateZ(${ss}deg)`;
+})
